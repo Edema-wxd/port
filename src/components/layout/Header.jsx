ca8d103eca85c525eb706204f8e695797944da3c
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-12-20 07:05:16 +0100

    first build of header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Header.module.scss";
 
 function Header() {
-	return <div>Header</div>;
+	return (
+		<div>
+			<img src="" alt="logo" />
+			<div className={style.linkarr}>
+				<Link to={"/"}>Home</Link>
+				<Link to={"/portfolio"}>Portfolio</Link>
+				<Link to={"/contact"}>Connect</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
