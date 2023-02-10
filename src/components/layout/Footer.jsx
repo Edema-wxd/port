4290f39c854e113edef590e3298cf2bdba80ad8f
Edema-wxd <Edema-wxd@users.noreply.github.com>
2023-02-10 16:53:02 +0100

    raw

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,11 @@ import style from "./Footer.module.css";
 
 function Footer() {
 	return (
-		<footer className={style.foot}>designed by Francis &copy; 2022</footer>
+		<footer className={style.foot}>
+			<hr className={style.fhr} />
+			Developed by Francis Woods &copy; Copyright 2021 -{" "}
+			{new Date().getFullYear()}. All right reserved.
+		</footer>
 	);
 }
 
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,9 +3,33 @@ import { Link } from "react-router-dom";
 import style from "./Header.module.css";
 
 function Header() {
+	const [openMenu, setOpenMenu] = useState(false);
+	const toggleMenu = () => {
+		setOpenMenu(!openMenu);
+	};
+
 	return (
 		<div className={style.header}>
-			<p className={style.linktxt}>Francis Woods</p>
+			<p className={style.linktxt}>Francis.wxd</p>
+
+			<div class={style.drop}>
+				<button onclick="myFunction()">Dropdown</button>
+				<div className={style.linkarrdrop} id="myDropdown">
+					<Link className={style.link} to={"/"}>
+						About
+					</Link>
+					<Link className={style.link} to={"/"}>
+						Services
+					</Link>
+					<Link className={style.link} to={"/portfolio"}>
+						Portfolio
+					</Link>
+					<Link className={style.link} to="#contact">
+						Contact
+					</Link>
+				</div>
+			</div>
+
 			<div className={style.linkarr}>
 				<Link className={style.link} to={"/"}>
 					About
