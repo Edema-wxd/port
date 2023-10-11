a5162e695da686891a0d7ae8ccd1be1e3a7a9684
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-10-11 14:30:30 +0100

    added menu btn

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,32 +1,62 @@
 import React from "react";
-//import { useState } from "react";
+import { useState } from "react";
 import style from "./Header.module.css";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Header() {
-	return (
-		<div className={style.header}>
-			<p className={style.linktxt}>Francis.wxd</p>
+  const [openMenu, setOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <div className={style.header}>
+      <p className={style.linktxt}>Francis.wxd</p>
 
-			<div className={style.drop}>
-				<button className={style.menubtn}>Dropdown</button>
-				<div className={style.linkarr} id="myDropdown">
-					<AnchorLink className={style.link} href="#hero">
-						About
-					</AnchorLink>
-					<AnchorLink className={style.link} href="#skills">
-						Skills
-					</AnchorLink>
-					<AnchorLink className={style.link} href="#portfolio">
-						Portfolio
-					</AnchorLink>
-					<AnchorLink className={style.link} href="#contact">
-						Contact
-					</AnchorLink>
-				</div>
-			</div>
-		</div>
-	);
+      <div className={style.drop}>
+        <div className={style.linkarr} id="myDropdown">
+          <AnchorLink className={style.link} href="#hero">
+            About
+          </AnchorLink>
+          <AnchorLink className={style.link} href="#skills">
+            Skills
+          </AnchorLink>
+          <AnchorLink className={style.link} href="#portfolio">
+            Portfolio
+          </AnchorLink>
+          <AnchorLink className={style.link} href="#contact">
+            Contact
+          </AnchorLink>
+        </div>
+        {openMenu ? (
+          <>
+            <div className={style.tit}>
+              <AiOutlineClose onClick={toggleMenu} />
+              <div className={style.Nslinks}>
+                <AnchorLink className={style.link} href="#hero">
+                  About
+                </AnchorLink>
+                <AnchorLink className={style.link} href="#skills">
+                  Skills
+                </AnchorLink>
+                <AnchorLink className={style.link} href="#portfolio">
+                  Portfolio
+                </AnchorLink>
+                <AnchorLink className={style.link} href="#contact">
+                  Contact
+                </AnchorLink>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div onClick={toggleMenu} className={style.menubtn}>
+            <BiMenu />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
