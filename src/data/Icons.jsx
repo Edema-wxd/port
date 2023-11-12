ea9a9d781089ed91944b9f72347eb97c903730ef
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-11-12 21:20:27 +0100

    added icons

--- src/data/Icons.jsx
+++ src/data/Icons.jsx
@@ -8,5 +8,15 @@ import js from "../../assets/js.svg";
 import oop from "../../assets/oop.svg";
 import code from "../../assets/code.svg";
 
-const Icons = [{ id: 1, title: "Django", image: django }];
+const Icons = [
+  { id: 1, title: "Django", image: django },
+  { id: 2, title: "Python", image: python },
+  { id: 3, title: "Git", image: git },
+  { id: 4, title: "Node.JS", image: nodejs },
+  { id: 5, title: "React", image: react },
+  { id: 6, title: "PostgreSQL", image: postgresql },
+  { id: 7, title: "Javascript", image: js },
+  { id: 8, title: "OOP", image: oop },
+  { id: 9, title: "Development", image: code },
+];
 export default Icons;
